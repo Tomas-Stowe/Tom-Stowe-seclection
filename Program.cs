eaadca74833c65d1b0272e4ccb1045cbdad42c64
Tomas-Stowe <Stowe_T@bryanston.co.uk>
2026-09-18 11:38:53 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+// Selection demo program
+
+using System;
+
+class MainClass {
+	public static void Main (string[] args) {
+	
+	// some conditions
+	bool thisIsTrue = true;
+	bool thisIsFalse = false;
+
+	// if
+
+
+	// if ... else
+
+
+
+	// if ... else if ... else
+
+
+
+	// combining conditions using and
+	
+	
+	
+	// combining conditions using or
+	}
+}
+
